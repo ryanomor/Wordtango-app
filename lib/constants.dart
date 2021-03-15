0047d4f9f346de61517196d6c3e8910346224934
Ryan Omoruyi <omoruyi.ryan@gmail.com>
2021-03-15 00:10:21 -0400

    fix wt orange

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
-const wtPrimaryColor = Color(0xffec7c44); // Wordtango orange
+const wtPrimaryColor =
+    Color(0xfff15a24); //Color(0xffec7c44); // Wordtango orange
